db72ff10f8d9e82a28799cb51d2c51fb3802826f
Santiago Balestero <santiagob@asmplatform.dev>
2022-07-30 16:53:19 -0300

    Add more data on product detail view

--- MeliChallenge2022/Views/DetailViewController.swift
+++ MeliChallenge2022/Views/DetailViewController.swift
@@ -13,7 +13,8 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
-    
+    @IBOutlet weak var quantityLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
     var product: ProductModel? {
         didSet {
             refresh()
@@ -24,14 +25,20 @@ class DetailViewController: UIViewController {
         loadViewIfNeeded()
         titleLabel.text = product?.title
         priceLabel.text = "\(product?.currency ?? "") \(product?.price ?? 0.0)"
+        quantityLabel.text = product?.quantity ?? 0 > 1 ? "\(product?.quantity ?? 0) disponibles"
+        : "\(product?.quantity ?? 0) disponible"
+        conditionLabel.text = "Producto " + "\(product?.condition == "new" ? "nuevo" : "usado")"
         imageView.sd_imageIndicator = SDWebImageActivityIndicator.medium
         imageView.sd_setImage(with: URL(string: product?.image ?? ""), placeholderImage: UIImage(named: "placeholder"))
     }
     
-//    @IBAction func onViewOnWebButtonTouchUpInside(_ sender: Any) {
-//        if let link = URL(string: product?.permalink ?? "") {
-//          UIApplication.shared.open(link)
-//        }
-//    }
-//    
+    @IBAction func onCloseButtonTap(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+        @IBAction func onViewOnWebButtonTouchUpInside(_ sender: Any) {
+        if let link = URL(string: product?.permalink ?? "") {
+          UIApplication.shared.open(link)
+        }
+    }
+    
 }
---
+++ MeliChallenge2022UITests/MeliChallenge2022ProductTableViewTest.swift
@@ -0,0 +1,13 @@
+//
+//  MeliChallenge2022ProductTableViewTest.swift
+//  MeliChallenge2022UITests
+//
+//  Created by Santiago Balestero on 30/7/22.
+//
+
+import XCTest
+
+class MeliChallenge2022ProductTableViewTest: XCTestCase {
+    
+    
+}
